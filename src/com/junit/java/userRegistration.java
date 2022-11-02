dfcfe50bd8d760e7a80d6236e275c6c132d4e245
sayrish <sayrrish1@gmail.com>
2022-11-02 11:18:25 +0530

    ValidOfPassward-Rule2

--- src/com/junit/java/userRegistration.java
+++ src/com/junit/java/userRegistration.java
@@ -15,7 +15,6 @@ public class userRegistration {
 		return matcher.matches();
 	}
 
-	
 	public boolean lastName(String lastname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
@@ -23,26 +22,32 @@ public class userRegistration {
 		return matcher.matches();
 	}
 
-	
 	public boolean email(String email) {
 		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
-	
+
 	public boolean mobileNumber(String number) {
 		String regex = "^(91)()[6-9]{1}[0-9]{9}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(number);
 		return matcher.matches();
 	}
-	
+
 	public boolean password(String password) {
 		String regex = "((?=.*[A-Z]).{8,})";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(password);
-		return matcher.
-	
-	
+		return matcher.matches();
+	}
+
+	public boolean password2(String password) {
+		String regex = "((?=.*[A-Z])(?=.*[0-9]).{8,})";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+
+	}
 }
